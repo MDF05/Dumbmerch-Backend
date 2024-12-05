a8ce29c4ef6b208f41d26acea1f1fae74126bd36
Muhammad Dava Fahreza <mdavafahreza05@gmail.com>
2024-12-05 16:08:14 +0700

    fix: cors origin frontedn

--- src/app.ts
+++ src/app.ts
@@ -23,9 +23,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/assets", express.static("./src/image"));
 
 const server = createServer(app);
+// origin: "http://localhost:5173",
 const io = new Server(server, {
   cors: {
-    // origin: "http://localhost:5173",
     origin: "https://dumbemerch-frontend.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   },
